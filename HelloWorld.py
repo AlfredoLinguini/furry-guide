91b3f2e7037fcc7a0e69b6b78e4b3510a22965fa
Linguini <107818726+AlfredoLinguini@users.noreply.github.com>
2022-06-30 22:02:06 +0000

    💀💀💀

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,7 @@
 print("Hello fellow pups, my name is RECO Mitchello and I'll ben your furry guide for two day?🐕")
-print("nigle")
+from time import sleep
+print("nigle") ; sleep(1.5)
+print("My name Micheal Inthebathroom Clareburt") ; sleep(1.5)
+print("And if you're reading this") ; sleep(0.4)
+print("I'm probably dead...") ; sleep(2)
+print("💀💀💀")
