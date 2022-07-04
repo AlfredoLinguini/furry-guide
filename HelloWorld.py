3975c876925a7218416a83b741c89adc50821a6c
Linguini <107818726+AlfredoLinguini@users.noreply.github.com>
2022-07-04 00:01:24 +0000

    continue on fetish

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,11 @@
+
 print("Hello fellow pups, my name is RECO Mitchello and I'll ben your furry guide for two day?🐕")
 from time import sleep
 print("nigle") ; sleep(1.5)
 print("My name Micheal Inthebathroom Clareburt") ; sleep(1.5)
 print("And if you're reading this") ; sleep(0.4)
 print("I'm probably dead...") ; sleep(2)
-print("💀💀💀")
+print("💀💀💀") ; sleep(2)
+
+fetish = input("What is your fetish?")
+if 
