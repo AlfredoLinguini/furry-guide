d1cbabf67a76a9afd7cc1c948700654f4f637bcb
Linguini <107818726+AlfredoLinguini@users.noreply.github.com>
2022-07-07 21:45:28 +0000

    lower and strip and furry

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,3 @@
-
 print("Hello fellow pups, my name is RECO Mitchello and I'll ben your furry guide for two day?🐕")
 from time import sleep
 print("nigle") ; sleep(1.5)
@@ -7,5 +6,22 @@ print("And if you're reading this") ; sleep(0.4)
 print("I'm probably dead...") ; sleep(2)
 print("💀💀💀") ; sleep(2)
 
-fetish = input("What is your fetish?")
-if fet
+#fetishes
+fetish = input("What is your fetish? ")
+fetish = fetish.lower()
+fetish = fetish.strip()
+if fetish == "gay":
+  print("Respect")
+elif fetish == "lesbian":
+  print("Respect")
+elif fetish == "bi":
+  print("Respect")
+elif fetish == "heterosexual":
+  print("normey")
+elif fetish == "feet":
+  print("YOOOOO SAME")
+elif fetish == "facesitting":
+  print("SO DO I")
+else:
+  print("don't care, didn't ask")
+
